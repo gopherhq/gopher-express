de98dbc75dfa81ef0048bd771ca4e6a20de1614e
Reilly Sweetland <esweetland@gmail.com>
2017-11-15 11:57:31 -0800

    callback url autoconfig for glitch

--- config.js
+++ config.js
@@ -7,13 +7,14 @@ const	extensionUrl = process.env.EXTENSION_URL;
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 const apiHost = process.env.API_HOST || 'https://www.gopher.email/';
+const redirectUrl = process.env.REDIRECT_URI || 'https://' + process.env.PROJECT_DOMAIN + '.glitch.me/auth/callback';
 
 module.exports = {
   extensionUrl: extensionUrl,
   apiHost: apiHost,
   clientId: clientId,
   clientSecret: clientSecret,
-  redirectUri: process.env.CALLBACK,
+  redirectUri: redirectUrl,
   tokenHost: apiHost,
   tokenPath: apiHost + 'api/v1/oauth2/access_token',
   authorizePath: apiHost + 'settings/oauth2_authorize',
