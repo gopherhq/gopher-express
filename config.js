eb39a856d0bd3f544c344e0a6b046cff2d4f18cc
Reilly Sweetland <esweetland@gmail.com>
2018-01-11 21:08:58 -0800

    move oauth scope to env with sensible defaults

--- config.js
+++ config.js
@@ -1,13 +1,20 @@
-if(!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
-  console.log('Please fill out the settings in the .env file to connect with Gopher');
+if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
+  console.log(
+    "Please fill out the settings in the .env file to connect with Gopher"
+  );
   process.exit();
 }
 
-const	extensionUrl = process.env.EXTENSION_URL;
+const extensionUrl = process.env.EXTENSION_URL;
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
-const apiHost = process.env.API_HOST || 'https://api.gopher.email/';
-const redirectUrl = process.env.REDIRECT_URI || 'https://' + process.env.PROJECT_DOMAIN + '.glitch.me/auth/callback';
+const apiHost = process.env.API_HOST || "https://api.gopher.email/";
+const redirectUrl =
+  process.env.REDIRECT_URI ||
+  "https://" + process.env.PROJECT_DOMAIN + ".glitch.me/auth/callback";
+const scope =
+  process.env.SCOPE ||
+  "get_user_info extension_manage_self manage_reminders read_reminders manage_logs read_logs read_tasks manage_tasks manage_extensions";
 
 module.exports = {
   extensionUrl: extensionUrl,
@@ -16,7 +23,7 @@ module.exports = {
   clientSecret: clientSecret,
   redirectUri: redirectUrl,
   tokenHost: apiHost,
-  tokenPath: apiHost + 'api/v1/oauth2/access_token',
-  authorizePath: apiHost + 'settings/oauth2_authorize',
-  scope: 'get_user_info extension_manage_self manage_reminders read_reminders manage_logs read_logs read_tasks manage_tasks manage_extensions'
-}
+  tokenPath: apiHost + "api/v1/oauth2/access_token",
+  authorizePath: apiHost + "settings/oauth2_authorize",
+  scope: scope
+};
